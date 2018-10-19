be1545094139777e49fd936e034145a414f502cb
LU WEI HSU <wiwi0322@amt.tw>
2018-10-19 17:44:57 +0800

    Complete GuessInput component render test for props success=false

--- src/GuessInput.js
+++ src/GuessInput.js
@@ -3,8 +3,35 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 const GuessInput = class extends Component {
+  propTypes = {
+    success: PropTypes.bool.isRequired
+  }
+
   render() {
-    return <div />
+    return (
+      <div data-test="cpn-guess-input">
+        {this.props.success ? null : (
+          <form className="form-inline">
+            <div className="form-group">
+              <input
+                data-test="elm-input"
+                id="word-guess-input"
+                type="text"
+                className="form-control"
+                placeholder="enter guess here."
+              />
+              <button
+                data-test="elm-submit-btn"
+                type="submit"
+                className="btn btn-primary"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
+    )
   }
 }
 
--- src/GuessInput.test.js
+++ src/GuessInput.test.js
@@ -4,6 +4,12 @@ import { shallow } from 'enzyme'
 import { findByTestAttr, storeFactory } from '../test/utils'
 import GuessInput from './GuessInput'
 
+const ELMENTS = {
+  component: 'cpn-guess-input',
+  input: 'elm-input',
+  submitButton: 'elm-submit-btn'
+}
+
 const setup = (state = {}) => {
   const store = storeFactory(state)
   const wrapper = shallow(<GuessInput store={store} />).dive()
@@ -12,14 +18,29 @@ const setup = (state = {}) => {
 
 describe('render', () => {
   describe('wrod has not been guessed', () => {
-    it('renders component without error', () => {})
-    it('renders input box', () => {})
-    it('renders submit button', () => {})
+    let wrapper
+    
+    beforeEach(() => {
+      const initialState = { success: false }
+      wrapper = setup(initialState)
+    })
+
+    it('renders component without error', () => {
+      const component = findByTestAttr(wrapper, ELMENTS.component)
+      expect(component.length).toBe(1)
+    })
+    it('renders input', () => {
+      const input = findByTestAttr(wrapper, ELMENTS.input)
+      expect(input.length).toBe(1)
+    })
+    it('renders submit button', () => {
+      const button = findByTestAttr(wrapper, ELMENTS.submitButton)
+      expect(button.length).toBe(1)
+    })
   })
   describe('wrod has been guessed', () => {
     it('renders component without error', () => {})
-    it('does not renders input box', () => {})
+    it('does not renders input', () => {})
     it('does not renders submit button', () => {})
   })
 })
-
