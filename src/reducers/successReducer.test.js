49fbaa55560de7cd47afb16ab62da74dd4fe6400
wiwimouse <wiwimouse@gmail.com>
2018-11-06 21:14:46 +0800

    update success reducer and its test with resetGame action creator

--- src/reducers/successReducer.test.js
+++ src/reducers/successReducer.test.js
@@ -10,3 +10,8 @@ it('returns state of `true` upon recieving an action of type `CORRECT_GUESS`', (
   const state = successReducer(undefined, { type: actionTypes.CORRECT_GUESS })
   expect(state).toBe(true)
 })
+
+it('retuns state of `false` when recieving an action of type `RESET_GAME`', () => {
+  const state = successReducer(true, { type: actionTypes.RESET_GAME })
+  expect(state).toBe(false)
+})
